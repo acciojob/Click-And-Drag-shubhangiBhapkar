94a6def838af23e1db6716ba3166b73bfb95e059
Shubhangi Bhapkar <shubhangibhapkar35@gmail.com>
2026-07-13 20:38:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// Your code here.
+// Your code her
+const boxes = document.querySelectorAll(".item");
+let isdragging = false;
+let currentbox;
+boxes.forEach((box)=>{
+	box.addEventListener("mousedown",(event)=>{
+		isdragging = true;
+		currentbox = box;
+	});
+
+	
+})
+document.addEventListener("mousemove",(event)=>{
+		if(isdragging){
+			currentbox.style.left = event.clientX + 'px';
+			currentbox.style.top = event.clientY + 'px';
+		}
+		
+	});
+
+	document.addEventListener("mouseup",(event)=>{
+		isdragging = false;
+		currentbox = null;
+	})
