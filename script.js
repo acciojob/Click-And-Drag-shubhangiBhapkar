7fc9d66f48d07e16109e6c78638a702f95b98353
Shubhangi Bhapkar <shubhangibhapkar35@gmail.com>
2026-07-14 10:21:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,48 @@
-const slider = document.querySelector(".items");
+const container = document.querySelector(".items");
+const items = document.querySelectorAll(".item");
 
-let isDown = false;
-let startX;
-let scrollLeft;
+let currentcube = null;
+let draggable = false;
 
-slider.addEventListener("mousedown", (e) => {
-    isDown = true;
+let ofsetX = 0;
+let ofsetY = 0;
 
-    // Mouse position inside the container
-    startX = e.pageX - slider.offsetLeft;
+items.forEach((item)=>{
+	item.addEventListener("mousedown", (e)=>{
+		currentcube = item;
+		draggable=true;
+		ofsetX = e.ofsetX;
+		ofsetY = e.ofsetY;
 
-    // Current scroll position
-    scrollLeft = slider.scrollLeft;
+		item.style.position = "absolute";
+	})
+	
 });
 
-slider.addEventListener("mouseleave", () => {
-    isDown = false;
+document.addEventListener("mousemove",(e)=>{
+	if (!isDragging) return;
+
+    const rect = container.getBoundingClientRect();
+
+    let x = e.clientX - rect.left - offsetX;
+    let y = e.clientY - rect.top - offsetY;
+
+    x = Math.max(
+        0,
+        Math.min(x, container.clientWidth - currentCube.offsetWidth)
+    );
+
+    y = Math.max(
+        0,
+        Math.min(y, container.clientHeight - currentCube.offsetHeight)
+    );
+
+    currentCube.style.left = x + "px";
+    currentCube.style.top = y + "px";
+
 });
 
-slider.addEventListener("mouseup", () => {
-    isDown = false;
-});
-
-slider.addEventListener("mousemove", (e) => {
-    if (!isDown) return;
-
-    e.preventDefault();
-
-    const x = e.pageX - slider.offsetLeft;
-    const walk = x - startX;
-
-    slider.scrollLeft = scrollLeft - walk;
-});
+document.addEventListener("mouseup",(e)=>{
+	currentcube = null;
+    draggable = false;
+})
